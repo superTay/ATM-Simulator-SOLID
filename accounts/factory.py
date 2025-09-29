28ee9161d70052c874c7adffb447823428035f23
Christian Marzal Della Rovere <infozenfusionhome@gmail.com>
2025-09-29 22:28:05 +0200

    chore: save changes on factory before branch switch

--- accounts/factory.py
+++ accounts/factory.py
@@ -4,6 +4,20 @@ from accounts.checking_account import CheckingAccount
 from accounts.credit_account import CreditAccount
 
 class AccountFactory:
+
+    """
+    A factory class responsible for creating instances of different types of accounts.
+
+    This class uses a static method to instantiate and return the appropriate account
+    object based on the provided account type and parameters.
+
+    Methods:
+        create_account(account_type: str, **kwargs) -> AccountManager:
+            Static method to create and return an account instance of the specified type.
+
+    Raises:
+        ValueError: If the account_type provided does not match any known account types.
+    """
     @staticmethod
     def create_account(account_type: str, **kwargs) -> AccountManager:
         if account_type == "savings":
